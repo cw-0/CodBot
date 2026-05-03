52fc5d750d26fa56cc1c0a66b1c9979fc4351c35
Caden Warren <caden.warren47@gmail.com>
2026-05-03 18:40:35 -0400

    Remove Timer On Select Loadout

--- src/State.cpp
+++ src/State.cpp
@@ -47,17 +47,17 @@ void State::checkInGame() {
           continue;
         }
       }
-      // Detect Loadout
-    } // Normal || Leniant
+    }
+
+    // Detect Loadout
     c = GetPixel(dng, LOADOUT_DETECTION_X, LOADOUT_DETECTION_Y);
     R = static_cast<int>(GetRValue(c));
     G = static_cast<int>(GetGValue(c));
     B = static_cast<int>(GetBValue(c));
-
-    if (R >= 105 && R <= 250) {   // >= 200 || 105
-      if (G >= 80 && G <= 195) {  // >= 155 || 80
-        if (B >= 30 && B <= 80) { // >= 50 || 30
-          std::this_thread::sleep_for(std::chrono::seconds(3));
+    // StrictDetect || LenientDetect
+    if (R >= 105 && R <= 250) {                     // >= 200 || 105
+      if (G >= 80 && G <= 195) {                    // >= 155 || 80
+        if (B >= 30 && B <= 80) {                   // >= 50 || 30
           SetCursorPos(LOADOUT_POSX, LOADOUT_POSY); // Loadout1 Coord
           INPUT inputs[2] = {};
           inputs[0].type = INPUT_MOUSE;
@@ -73,99 +73,112 @@ void State::checkInGame() {
                             std::to_string(LOADOUT_POSX) + ", " +
                             std::to_string(LOADOUT_POSY) + ")";
           Logger::write(msg.c_str());
+          continue;
         }
       }
+    } // End of Detect Loadout
+
+    // Detect if Kicked
+    c = GetPixel(dng, KICKED_DETECTION_X, KICKED_DETECTION_Y);
+    R = static_cast<int>(GetRValue(c));
+    G = static_cast<int>(GetGValue(c));
+    B = static_cast<int>(GetBValue(c));
+    if (R >= 105 && R <= 250) {
+      if (G >= 80 && G <= 195) {
+        if (B >= 30 && B <= 80) {
+        }
+
+        std::string msg = "In Game Pixel NOT Found: (" + std::to_string(R) +
+                          ", " + std::to_string(G) + ", " + std::to_string(B) +
+                          ")";
+        Logger::write(msg.c_str());
+        MISSED_IN_GAME_CHECK++;
+        std::string missCheckMsg = "Missed In Game Checks In a Row Count: " +
+                                   std::to_string(MISSED_IN_GAME_CHECK);
+        Logger::write(missCheckMsg.c_str());
+        if (MISSED_IN_GAME_CHECK >= 3) {
+          IN_GAME = false;
+          IS_SOFT_PAUSED = true;
+        }
+      }
+      std::cout << "In Game Check Thread Terminated\n";
+      Logger::write("In Game Check Thread Terminated");
     }
 
-    std::string msg = "In Game Pixel NOT Found: (" + std::to_string(R) + ", " +
-                      std::to_string(G) + ", " + std::to_string(B) + ")";
-    Logger::write(msg.c_str());
-    MISSED_IN_GAME_CHECK++;
-    std::string missCheckMsg = "Missed In Game Checks In a Row Count: " +
-                               std::to_string(MISSED_IN_GAME_CHECK);
-    Logger::write(missCheckMsg.c_str());
-    if (MISSED_IN_GAME_CHECK >= 3) {
-      IN_GAME = false;
-      IS_SOFT_PAUSED = true;
-    }
-  }
-  std::cout << "In Game Check Thread Terminated\n";
-  Logger::write("In Game Check Thread Terminated");
-}
-
-void State::checkState() {
-  Logger::write("Check State Called", true);
-  if (!IN_GAME) {
-    IS_SOFT_PAUSED = true;
-  }
-  if (IS_PAUSED || IS_SOFT_PAUSED) {
-    Utils::clearTerm();
-    std::cout << (IS_PAUSED
-                      ? "Manually Paused\n"
-                      : "Waiting For Loadout Screen or In Game Detection\n");
-    Logger::write((IS_PAUSED
-                       ? "Manually Paused\n"
+    void State::checkState() {
+      Logger::write("Check State Called", true);
+      if (!IN_GAME) {
+        IS_SOFT_PAUSED = true;
+      }
+      if (IS_PAUSED || IS_SOFT_PAUSED) {
+        Utils::clearTerm();
+        std::cout
+            << (IS_PAUSED
+                    ? "Manually Paused\n"
+                    : "Waiting For Loadout Screen or In Game Detection\n");
+        Logger::write(
+            (IS_PAUSED ? "Manually Paused\n"
                        : "Waiting For Loadout Screen or In Game Detection\n"));
-    while (IS_PAUSED || IS_SOFT_PAUSED) {
-      // CLICK ON LOADOUT POS
-      // if (!IS_PAUSED) {
-      //   // ONLY CLICK ON LOADOUT POS IF IT HAS BEEN IN GAME ONCE ALREADY
-      //   // if (!RAN_ONCE) {
-      //   //
-      //   //   continue;
-      //   // }
-      //
-      //   std::this_thread::sleep_for(std::chrono::seconds(3));
-      //   SetCursorPos(LOADOUT_POSX, LOADOUT_POSY); // Loadout1 Coord
-      //   INPUT inputs[2] = {};
-      //   inputs[0].type = INPUT_MOUSE;
-      //   inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-      //   inputs[1].type = INPUT_MOUSE;
-      //   inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
-      //   SendInput(2, inputs, sizeof(INPUT));
-      //
-      //   if (SEND_VERBOSE) { // no need to create msg if not sending
-      //     std::string msg = "Clicked Loadout Position (" +
-      //                       std::to_string(LOADOUT_POSX) + ", " +
-      //                       std::to_string(LOADOUT_POSY) + ")";
-      //     Logger::write(msg.c_str(), true);
-      //   }
-      // };
+        while (IS_PAUSED || IS_SOFT_PAUSED) {
+          // CLICK ON LOADOUT POS
+          // if (!IS_PAUSED) {
+          //   // ONLY CLICK ON LOADOUT POS IF IT HAS BEEN IN GAME ONCE ALREADY
+          //   // if (!RAN_ONCE) {
+          //   //
+          //   //   continue;
+          //   // }
+          //
+          //   std::this_thread::sleep_for(std::chrono::seconds(3));
+          //   SetCursorPos(LOADOUT_POSX, LOADOUT_POSY); // Loadout1 Coord
+          //   INPUT inputs[2] = {};
+          //   inputs[0].type = INPUT_MOUSE;
+          //   inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+          //   inputs[1].type = INPUT_MOUSE;
+          //   inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+          //   SendInput(2, inputs, sizeof(INPUT));
+          //
+          //   if (SEND_VERBOSE) { // no need to create msg if not sending
+          //     std::string msg = "Clicked Loadout Position (" +
+          //                       std::to_string(LOADOUT_POSX) + ", " +
+          //                       std::to_string(LOADOUT_POSY) + ")";
+          //     Logger::write(msg.c_str(), true);
+          //   }
+          // };
+        }
+        Utils::clearTerm();
+        std::cout << "Resumed.\n";
+        return;
+      } // END IF IS PAUSED
+
+    } // END CHECK STATE
+
+    void State::changeState() {
+      needThread = true;
+      while (needThread) {
+        if (GetKeyState(0xA4) & 0x8000 && !IS_PAUSED) { // Left Alt
+          IS_PAUSED = true;
+          Logger::write("Paused : Hot Key Pressed");
+          std::cout << "PAUSING\n";
+        }
+
+        if (GetKeyState(0xA5) & 0x8000 && IS_PAUSED) { // Right Alt
+          IS_PAUSED = false;
+          Logger::write("Resuming : Hot Key Pressed");
+          std::cout << "RESUMING\n";
+        }
+
+        if (GetKeyState(0xA3) & 0x8000) { // Right Ctrl Stop Bot
+          Utils::clearTerm();
+          std::cout << "Exiting\n";
+          Logger::write("Exiting : Hot Key Pressed");
+          exit(0);
+        }
+      }
+      std::cout << "Listener Thread Terminated\n";
+      Logger::write("Hot Key Thread Terminated");
     }
-    Utils::clearTerm();
-    std::cout << "Resumed.\n";
-    return;
-  } // END IF IS PAUSED
 
-} // END CHECK STATE
-
-void State::changeState() {
-  needThread = true;
-  while (needThread) {
-    if (GetKeyState(0xA4) & 0x8000 && !IS_PAUSED) { // Left Alt
-      IS_PAUSED = true;
-      Logger::write("Paused : Hot Key Pressed");
-      std::cout << "PAUSING\n";
+    void State::endThreads() {
+      needThread = false;
+      Logger::write("All Threads Ended");
     }
-
-    if (GetKeyState(0xA5) & 0x8000 && IS_PAUSED) { // Right Alt
-      IS_PAUSED = false;
-      Logger::write("Resuming : Hot Key Pressed");
-      std::cout << "RESUMING\n";
-    }
-
-    if (GetKeyState(0xA3) & 0x8000) { // Right Ctrl Stop Bot
-      Utils::clearTerm();
-      std::cout << "Exiting\n";
-      Logger::write("Exiting : Hot Key Pressed");
-      exit(0);
-    }
-  }
-  std::cout << "Listener Thread Terminated\n";
-  Logger::write("Hot Key Thread Terminated");
-}
-
-void State::endThreads() {
-  needThread = false;
-  Logger::write("All Threads Ended");
-}
